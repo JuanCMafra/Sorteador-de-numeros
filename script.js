191a3a816943490175bb9849d1a50a465e3d737f
Mafra <juan.carlos2113@hotmail.com>
2026-02-23 06:59:19 -0300

    fixing draw button

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const results = document.querySelectorAll("#results li")
 const button = document.getElementById("draw-again")
 const wrapper = document.querySelector("#second")
-
 // informa ao CSS quantos itens existem
 results.forEach(li => {
   li.style.setProperty("--total", results.length)
 })
-
 // espera a animação terminar
 const animationDuration = 1000 // ms
-
 setTimeout(() => {
   button.classList.add("show")
   wrapper.classList.add("shift-up")
 }, animationDuration)
+
+
